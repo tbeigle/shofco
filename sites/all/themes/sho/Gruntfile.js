181524867d1d83bd8dd9e3e243e45977b7873f45
James <james@transitauthorityfigures.com>
2014-04-18 16:24:14 -0400

    styling

--- sites/all/themes/sho/Gruntfile.js
+++ sites/all/themes/sho/Gruntfile.js
@@ -26,6 +26,6 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['less','watch']);
 
 }
