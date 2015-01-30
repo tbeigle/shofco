d7becd4f7efad26f70cac3675c0981e93f1ef573
Tom Beigle <tbeigle@gmail.com>
2015-01-30 10:43:40 -0600

    SHC-15: Added a setting for toggling autoplay settings of the homepage slider.

--- sites/all/themes/sho/theme-settings.php
+++ sites/all/themes/sho/theme-settings.php
@@ -13,4 +13,11 @@ function sho_form_system_theme_settings_alter(&$form, &$form_state) {
   if ($url = theme_get_setting('donate_url')) {
     $form['donate_url']['#default_value'] = $url;
   }
+  
+  $form['home_slider_autoplay'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Autoplay Home Slider'),
+    '#description' => t('If checked, the slider on the homepage will start sliding automatically. Otherwise, the slides will only animate in response to click/touch actions.'),
+    '#default_value' => theme_get_setting('home_slider_autoplay'),
+  );
 }
